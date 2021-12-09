0c8643d5a9db2368d8e86d445c7cd62ae75b00a0
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-12-08 20:11:31 -0500

    fixed formatting

--- src/main/java/models/DarkSoulsBosses.java
+++ src/main/java/models/DarkSoulsBosses.java
@@ -12,24 +12,24 @@ public class DarkSoulsBosses {
     public static void main(String[] args) {
         DaoConcreteClass bosses = new DaoConcreteClass();
 
-//        bossOutput(bosses.getBossByID(3));
-//
-//        bossListOutput(bosses.findAllBosses());
-//
-//        Boss exampleBossOne = new Boss("Soul of Cinder",
-//                "Kiln of the First Flame",
-//                10766, "Dark",
-//                "Bleed",
-//                100000);
-//        Boss createdBoss = bosses.create(exampleBossOne);
-//        bossOutput(createdBoss);
-//
+        bossOutput(bosses.getBossByID(3));
+
+        bossListOutput(bosses.findAllBosses());
+
+        Boss exampleBossOne = new Boss("Soul of Cinder",
+                "Kiln of the First Flame",
+                10766, "Dark",
+                "Bleed",
+                100000);
+        Boss createdBoss = bosses.create(exampleBossOne);
+        bossOutput(createdBoss);
+
         Boss exampleBossTwo = bosses.getBossByID(1);
         exampleBossTwo.setResistances("Dark");
         bossOutput(bosses.update(exampleBossTwo));
-//
-//        bosses.delete(createdBoss.getId());
-//        bossListOutput(bosses.findAllBosses());
+
+        bosses.delete(createdBoss.getId());
+        bossListOutput(bosses.findAllBosses());
     }
 
 
